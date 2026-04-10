2e6de69efadd6437c04d7fde0902c170e90abd3d
Abhishek <abhishek@suyagya.com>
2026-04-10 11:17:49 +0530

    add gtm code in app file

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -2,6 +2,13 @@
 <html lang="en">
 
 <head>
+    {{-- ✅ 1. Google Tag Manager (Script) - <head> के सबसे ऊपर --}}
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MLSST65C');</script>
+    {{-- End Google Tag Manager --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -622,6 +629,10 @@
             }
         }
     </style>
+    {{-- ✅ 2. Google Tag Manager (noscript) - <body> के तुरंत बाद --}}
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLSST65C"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    {{-- End Google Tag Manager (noscript) --}}
 
     {{-- ⚙️ SCRIPTS --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
